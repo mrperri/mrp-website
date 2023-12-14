5c0afb1cc6f7517878b7a05fc6978b9bc12c88a8
MarioP520 <mrp106@pitt.edu>
2023-12-13 23:35:03 -0500

    layout init changes (not working)

---
+++ src/components/layout/fixedlayout.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
+
+@Component({
+  selector: 'layout',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  template: `
+    <nz-layout>
+      <nz-header>
+        <div class="logo"></div>
+        <ul nz-menu nzTheme="dark" nzMode="horizontal">
+          <li nz-menu-item>nav 1</li>
+          <li nz-menu-item>nav 2</li>
+          <li nz-menu-item>nav 3</li>
+        </ul>
+      </nz-header>
+      <nz-content>
+        <div class="inner-content">Deez Content</div>
+      </nz-content>
+      <nz-footer>Mario Perri</nz-footer>
+    </nz-layout>
+  `,
+  styles: [
+    `
+      .logo {
+        width: 120px;
+        height: 31px;
+        background: rgba(255, 255, 255, 0.2);
+        margin: 16px 24px 16px 0;
+        float: left;
+      }
+
+      [nz-menu] {
+        line-height: 64px;
+      }
+
+      nz-content {
+        padding: 0 50px;
+      }
+
+      nz-footer {
+        text-align: center;
+      }
+
+      .inner-content {
+        background: #fff;
+        padding: 24px;
+        min-height: 280px;
+      }
+    `
+  ]
+})
+export class NzDemoLayoutTopComponent {}
