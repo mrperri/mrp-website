3331745fbfcf4b9dce366b61e77421099ac55c47
mrperri <mrp106@pitt.edu>
2024-07-03 15:09:53 -0400

    Github action created
    replaced bash script with github action

--- scripts/build_prod.sh
+++ scripts/build_prod.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ng build --configuration=production --base-href=https://mariop520.github.io/
+ng build --configuration=production --base-href=https://mrperri.github.io/
 rm -rf ../docs
 cp -r dist/browser ../docs
 cp ../docs/index.html ../docs/404.html
