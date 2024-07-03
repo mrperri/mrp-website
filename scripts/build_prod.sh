f665919dafb1c8b9985bc31ba18801a059962658
mrperri <mrperri@users.noreply.github.com>
2024-07-03 19:44:17 +0000

    deploy: 6be174489639a26fff9a999e026ee1cc8e6bae10

--- scripts/build_prod.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-ng build --configuration=production --base-href=https://mariop520.github.io/
-rm -rf ../docs
-cp -r dist/browser ../docs
-cp ../docs/index.html ../docs/404.html
