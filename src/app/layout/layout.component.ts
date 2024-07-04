710fd3f129102bed406aafa2db36a3cc743c9615
mrperri <mrp106@pitt.edu>
2024-07-04 18:40:21 -0400

    floated menu items to right in nav bar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet, RouterLink, RouterLinkActive } from "@angular/router";
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 
 
@@ -12,7 +17,18 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 @Component({
   selector: 'app-site-layout',
   standalone: true,
-  imports: [CommonModule, NzLayoutModule, NzMenuModule, NzBreadCrumbModule, RouterOutlet, RouterLink, RouterLinkActive,NzIconModule],
+  
+  imports: [
+
+    CommonModule, 
+    NzLayoutModule, 
+    NzMenuModule, 
+    NzBreadCrumbModule, 
+    RouterOutlet, 
+    RouterLink, 
+    RouterLinkActive,
+    NzIconModule, 
+    NzSpaceModule ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
