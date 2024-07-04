c59d4666853a99a0049781e1799e918962350d53
mrperri <mrp106@pitt.edu>
2024-07-04 16:59:38 -0400

    Added skeleton components for planned routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { SiteLayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { EducationComponent } from './education/education.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home', component: HomeComponent },
   { path: 'education', component: EducationComponent },
-  { path: 'experience', component: EducationComponent },  //TODO change component
-  { path: 'contact-me', component: EducationComponent },  //TODO change component
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact-me', component: ContactMeComponent },
   { path: '**', pathMatch: 'full', redirectTo: ''}
 ];
