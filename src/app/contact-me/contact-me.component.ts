151501d112a56ed63b87b805529dcfc4a44070d0
mrperri <mrp106@pitt.edu>
2024-07-07 23:10:20 -0400

    Added Grid sample to contact page

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+
 
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [NzFlexModule, NzDividerModule],
+  imports: [NzFlexModule, NzDividerModule, NzGridModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
