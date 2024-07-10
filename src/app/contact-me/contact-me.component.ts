a19d2d135c0f2f22991b4b4991083c007f697e19
mrperri <mrp106@pitt.edu>
2024-07-09 22:35:21 -0400

    Cleaned up contact page
    added contact information with links, and image of self (not committed yet)

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
 
 
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [NzFlexModule, NzDividerModule, NzGridModule],
+  imports: [NzFlexModule, NzDividerModule, NzGridModule, NzIconModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
